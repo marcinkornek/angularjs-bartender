5e039436e7cf398a49819d87b65f241593f6ac16
marcinkornek <marcinkornek@gmail.com>
2014-11-25 14:11:13 +0100

    Added order form min amount validation + error i18n

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -4,4 +4,8 @@ class OrderDetail < ActiveRecord::Base
 
   validates :orderable_id,    presence: true
   validates :orderable_type,  presence: true
+
+  validates :order,   presence: true
+  validates :amount,  presence: true,
+                      numericality: { greater_than: 0 }
 end
